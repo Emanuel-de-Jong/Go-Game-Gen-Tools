ebe4d23909695b65bce1ab176d16d0436ccc0230
Emanuel-de-Jong <emanueldejong@hotmail.com>
2022-10-07 13:08:26 +0200

    Func to log all func calls

--- client/js/debug.js
+++ client/js/debug.js
@@ -6,8 +6,59 @@ debug.testButtonClickListener = function() {
     console.log();
 };
 
+debug.logAllFuncCalls = function() {
+    let objs = [
+        board,
+        custom,
+        debug,
+        server,
+        settings,
+        stats,
+        utils,
+    ];
+    
+    for (let i=0; i<objs.length; i++) {
+        let obj = objs[i];
+        let funcNames = Object.getOwnPropertyNames(obj).filter(item => typeof obj[item] === 'function');
+        for (let j=0; j<funcNames.length; j++) {
+            let funcName = funcNames[j];
+
+            obj[funcName] = (function() {
+                let cachedFunc = obj[funcName];
+                
+                return function() {
+                    let log = funcName + "(";
+                    for (let x=0; x<arguments.length; x++) {
+                        let argument = arguments[x];
+                        switch (typeof argument) {
+                            case "object":
+                                log += "obj";
+                                break;
+                            case "function":
+                                log += "func";
+                                break;
+                            default:
+                                log += argument;
+                        }
+
+                        if (x != arguments.length - 1) {
+                            log += ", ";
+                        }
+                    }
+                    log += ")";
+                    console.log(log);
+
+                    let result = cachedFunc.apply(this, arguments);
+                    return result;
+                }
+            })();
+        }
+    }
+};
+
 (function () {
 
     debug.testButton.addEventListener("click", debug.testButtonClickListener);
+    // debug.logAllFuncCalls();
 
 })();
