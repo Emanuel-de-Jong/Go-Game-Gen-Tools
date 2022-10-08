595b0f71c7782769b4ad57edea849fde4eeeb675
Emanuel-de-Jong <emanueldejong@hotmail.com>
2022-10-08 11:31:02 +0200

    Make ScoreChart config into smaller const objects

--- client/js/classes/ScoreChart.js
+++ client/js/classes/ScoreChart.js
@@ -1,125 +1,132 @@
 class ScoreChart {
 
+    DATA = {
+        datasets: [
+            {
+                label: "Winrate",
+                pointRadius: 1,
+                borderColor: "rgb(0, 255, 0)",
+                backgroundColor: "rgba(0, 255, 0, 0.3)",
+                // fill: {
+                //     target: { value: 50 },
+                // },
+            },
+            {
+                label: "Score",
+                yAxisID: 'y1',
+                pointRadius: 1,
+                borderColor: "rgb(0, 0, 255)",
+                backgroundColor: "rgba(0, 0, 255, 0.3)",
+                // fill: {
+                //     target: { value: 0 },
+                // },
+            },
+        ],
+    };
+
+    PLUGINS = {
+        legend: {
+            onClick: (event, legendItem, legend) => {
+                const datasets = legend.legendItems.map((dataset, index) => {
+                    return dataset.text;
+                });
+                const index = datasets.indexOf(legendItem.text);
+                if (legend.chart.isDatasetVisible(index) === true) {
+                    legend.chart.hide(index);
+                } else {
+                    legend.chart.show(index);
+                }
+            },
+            labels: {
+                generateLabels: (chart) => {
+                    let visibility = [];
+                    for (let i=0; i<chart.data.datasets.length; i++) {
+                        if (chart.isDatasetVisible(i) === false) {
+                            visibility.push(true);
+                        } else {
+                            visibility.push(false);
+                        }
+                    }
+                    return chart.data.datasets.map(
+                        (dataset, index) => ({
+                            text: dataset.label + (dataset.data.length ? ": " + dataset.data.slice(-1) : ""),
+                            fillStyle: dataset.backgroundColor,
+                            strokeStyle: dataset.borderColor,
+                            hidden: visibility[index]
+                        })
+                    )
+                }
+            },
+        },
+    };
+
+    SCALES = {
+        y: {
+            suggestedMin: 45,
+            suggestedMax: 55,
+            title: {
+                display: true,
+                text: "Winrate",
+            },
+            afterDataLimits: function(axis) {
+                let maxDiff = axis.max - 50;
+                let minDiff = 50 - axis.min;
+                if (maxDiff > minDiff) {
+                    axis.min = 50 - maxDiff;
+                } else if (minDiff > maxDiff) {
+                    axis.max = 50 + minDiff;
+                }
+            },
+        },
+        y1: {
+            position: "right",
+            suggestedMin: -2,
+            suggestedMax: 2,
+            title: {
+                display: true,
+                text: "Score",
+            },
+            grid: {
+                // drawOnChartArea: false,
+                color: function(context) {
+                    if (context.tick.value == 0) return "#000000";
+                    return "#ffffff";
+                },
+            },
+            afterDataLimits: function(axis) {
+                if (axis.max > axis.min * -1) {
+                    axis.min = axis.max * -1;
+                } else if (axis.min * -1 > axis.max) {
+                    axis.max = axis.min * -1;
+                }
+            },
+        },
+    };
+
+    CONFIG = {
+        type: "line",
+        data: this.DATA,
+        options: {
+            responsive: true,
+            interaction: {
+                intersect: false,
+                mode: "index",
+            },
+            animation: {
+                duration: 0,
+            },
+            plugins: this.PLUGINS,
+            scales: this.SCALES,
+        },
+    };
+
     chart;
     labels;
     winrates;
     scores;
 
     constructor(element) {
-        this.chart = new Chart(element, {
-            type: "line",
-            data: {
-                datasets: [
-                    {
-                        label: "Winrate",
-                        pointRadius: 1,
-                        borderColor: "rgb(0, 255, 0)",
-                        backgroundColor: "rgba(0, 255, 0, 0.3)",
-                        // fill: {
-                        //     target: { value: 50 },
-                        // },
-                    },
-                    {
-                        label: "Score",
-                        yAxisID: 'y1',
-                        pointRadius: 1,
-                        borderColor: "rgb(0, 0, 255)",
-                        backgroundColor: "rgba(0, 0, 255, 0.3)",
-                        // fill: {
-                        //     target: { value: 0 },
-                        // },
-                    },
-                ],
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        onClick: (event, legendItem, legend) => {
-                            const datasets = legend.legendItems.map((dataset, index) => {
-                                return dataset.text;
-                            });
-                            const index = datasets.indexOf(legendItem.text);
-                            if (legend.chart.isDatasetVisible(index) === true) {
-                                legend.chart.hide(index);
-                            } else {
-                                legend.chart.show(index);
-                            }
-                        },
-                        labels: {
-                            generateLabels: (chart) => {
-                                let visibility = [];
-                                for (let i=0; i<chart.data.datasets.length; i++) {
-                                    if (chart.isDatasetVisible(i) === false) {
-                                        visibility.push(true);
-                                    } else {
-                                        visibility.push(false);
-                                    }
-                                }
-                                return chart.data.datasets.map(
-                                    (dataset, index) => ({
-                                        text: dataset.label + (dataset.data.length ? ": " + dataset.data.slice(-1) : ""),
-                                        fillStyle: dataset.backgroundColor,
-                                        strokeStyle: dataset.borderColor,
-                                        hidden: visibility[index]
-                                    })
-                                )
-                            }
-                        },
-                    },
-                },
-                interaction: {
-                    intersect: false,
-                    mode: "index",
-                },
-                animation: {
-                    duration: 0,
-                },
-                scales: {
-                    y: {
-                        suggestedMin: 45,
-                        suggestedMax: 55,
-                        title: {
-                            display: true,
-                            text: "Winrate",
-                        },
-                        afterDataLimits: function(axis) {
-                            let maxDiff = axis.max - 50;
-                            let minDiff = 50 - axis.min;
-                            if (maxDiff > minDiff) {
-                                axis.min = 50 - maxDiff;
-                            } else if (minDiff > maxDiff) {
-                                axis.max = 50 + minDiff;
-                            }
-                        },
-                    },
-                    y1: {
-                        position: "right",
-                        suggestedMin: -2,
-                        suggestedMax: 2,
-                        title: {
-                            display: true,
-                            text: "Score",
-                        },
-                        grid: {
-                            // drawOnChartArea: false,
-                            color: function(context) {
-                                if (context.tick.value == 0) return "#000000";
-                                return "#ffffff";
-                            },
-                        },
-                        afterDataLimits: function(axis) {
-                            if (axis.max > axis.min * -1) {
-                                axis.min = axis.max * -1;
-                            } else if (axis.min * -1 > axis.max) {
-                                axis.max = axis.min * -1;
-                            }
-                        },
-                    },
-                },
-            },
-        });
-
+        this.chart = new Chart(element, this.CONFIG);
         this.labels = this.chart.data.labels;
         this.winrates = this.chart.data.datasets[0].data;
         this.scores = this.chart.data.datasets[1].data;
