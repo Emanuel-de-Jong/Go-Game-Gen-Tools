1b1b8f51ed8380d21de8ec3d819e3ee9a82a386f
Emanuel-de-Jong <emanueldejong@hotmail.com>
2022-10-21 12:23:32 +0200

    Move board.fillCorners to main (because main isn't big enough yet smh my head)

--- client/js/board.js
+++ client/js/board.js
@@ -155,52 +155,6 @@ board.playPlaceStoneAudio = function() {
 	board.placeStoneAudios[placeStoneAudioIndex].play();
 };
 
-board.fillCorners = function() {
-	let cornerOptions = [
-		{ c44: {x:4,y:4}, c34: {x:3,y:4}, c43: {x:4,y:3}, c33: {x:3,y:3}, c45: {x:4,y:5}, c54: {x:5,y:4}, c35: {x:3,y:5}, c53: {x:5,y:3} },
-		{ c44: {x:16,y:4}, c34: {x:17,y:4}, c43: {x:16,y:3}, c33: {x:17,y:3}, c45: {x:16,y:5}, c54: {x:15,y:4}, c35: {x:17,y:5}, c53: {x:15,y:3} },
-		{ c44: {x:4,y:16}, c34: {x:3,y:16}, c43: {x:4,y:17}, c33: {x:3,y:17}, c45: {x:4,y:15}, c54: {x:5,y:16}, c35: {x:3,y:15}, c53: {x:5,y:17} },
-		{ c44: {x:16,y:16}, c34: {x:17,y:16}, c43: {x:16,y:17}, c33: {x:17,y:17}, c45: {x:16,y:15}, c54: {x:15,y:16}, c35: {x:17,y:15}, c53: {x:15,y:17} },
-	];
-	cornerOptions = utils.shuffleArray(cornerOptions);
-
-	let coords = [];
-	let totalCornerChance = (settings.cornerSwitch44 ? settings.cornerChance44 : 0) +
-		(settings.cornerSwitch34 ? settings.cornerChance34 : 0) +
-		(settings.cornerSwitch33 ? settings.cornerChance33 : 0) +
-		(settings.cornerSwitch45 ? settings.cornerChance45 : 0) +
-		(settings.cornerSwitch35 ? settings.cornerChance35 : 0);
-	for (let i=0; i<4; i++) {
-		let coord;
-		let cornerTypeRange = 0;
-		let rndCornerType = utils.randomInt(totalCornerChance);
-		let rndCornerSide = utils.randomInt(2);
-		if (settings.cornerSwitch44 &&
-				rndCornerType < (cornerTypeRange = cornerTypeRange + settings.cornerChance44)) {
-			coord = cornerOptions[i].c44;
-		}
-		else if (settings.cornerSwitch34 &&
-				rndCornerType < (cornerTypeRange = cornerTypeRange + settings.cornerChance34)) {
-			coord = rndCornerSide ? cornerOptions[i].c34 : cornerOptions[i].c43;
-		}
-		else if (settings.cornerSwitch33 &&
-				rndCornerType < (cornerTypeRange = cornerTypeRange + settings.cornerChance33)) {
-			coord = cornerOptions[i].c33;
-		}
-		else if (settings.cornerSwitch45 &&
-				rndCornerType < (cornerTypeRange = cornerTypeRange + settings.cornerChance45)) {
-			coord = rndCornerSide ? cornerOptions[i].c45 : cornerOptions[i].c54;
-		}
-		else {
-			coord = rndCornerSide ? cornerOptions[i].c35 : cornerOptions[i].c53;
-		}
-
-		coords.push(coord);
-	}
-
-	return coords;
-};
-
 board.getColor = function() {
 	let currentMove = board.editor.getCurrent();
 	if (currentMove.move != null) {
--- client/js/main.js
+++ client/js/main.js
@@ -38,7 +38,7 @@ main.createPreMoves = async function() {
 					settings.cornerSwitch45 ||
 					settings.cornerSwitch35) {
 				let cornerCount = preMovesLeft < 4 ? preMovesLeft : 4;
-				let cornerCoords = board.fillCorners();
+				let cornerCoords = main.fillCorners(cornerCount);
 				for (let i=0; i<cornerCount; i++) {
 					let suggestion = await server.analyzeMove(cornerCoords[i]);
 					if (main.isPreMovesStopped) break;
@@ -71,6 +71,53 @@ main.stopPreMovesButtonClickListener = function() {
 	main.isPreMovesStopped = true;
 };
 main.stopPreMovesButton.addEventListener("click", main.stopPreMovesButtonClickListener);
+
+main.fillCorners = function(cornerCount) {
+	let cornerOptions = [
+		{ c44: {x:4,y:4}, c34: {x:3,y:4}, c43: {x:4,y:3}, c33: {x:3,y:3}, c45: {x:4,y:5}, c54: {x:5,y:4}, c35: {x:3,y:5}, c53: {x:5,y:3} },
+		{ c44: {x:16,y:4}, c34: {x:17,y:4}, c43: {x:16,y:3}, c33: {x:17,y:3}, c45: {x:16,y:5}, c54: {x:15,y:4}, c35: {x:17,y:5}, c53: {x:15,y:3} },
+		{ c44: {x:4,y:16}, c34: {x:3,y:16}, c43: {x:4,y:17}, c33: {x:3,y:17}, c45: {x:4,y:15}, c54: {x:5,y:16}, c35: {x:3,y:15}, c53: {x:5,y:17} },
+		{ c44: {x:16,y:16}, c34: {x:17,y:16}, c43: {x:16,y:17}, c33: {x:17,y:17}, c45: {x:16,y:15}, c54: {x:15,y:16}, c35: {x:17,y:15}, c53: {x:15,y:17} },
+	];
+	cornerOptions = utils.shuffleArray(cornerOptions);
+
+	let coords = [];
+	let totalCornerChance = (settings.cornerSwitch44 ? settings.cornerChance44 : 0) +
+		(settings.cornerSwitch34 ? settings.cornerChance34 : 0) +
+		(settings.cornerSwitch33 ? settings.cornerChance33 : 0) +
+		(settings.cornerSwitch45 ? settings.cornerChance45 : 0) +
+		(settings.cornerSwitch35 ? settings.cornerChance35 : 0);
+	
+	for (let i=0; i<cornerCount; i++) {
+		let coord;
+		let cornerTypeRange = 0;
+		let rndCornerType = utils.randomInt(totalCornerChance);
+		let rndCornerSide = utils.randomInt(2);
+		if (settings.cornerSwitch44 &&
+				rndCornerType < (cornerTypeRange = cornerTypeRange + settings.cornerChance44)) {
+			coord = cornerOptions[i].c44;
+		}
+		else if (settings.cornerSwitch34 &&
+				rndCornerType < (cornerTypeRange = cornerTypeRange + settings.cornerChance34)) {
+			coord = rndCornerSide ? cornerOptions[i].c34 : cornerOptions[i].c43;
+		}
+		else if (settings.cornerSwitch33 &&
+				rndCornerType < (cornerTypeRange = cornerTypeRange + settings.cornerChance33)) {
+			coord = cornerOptions[i].c33;
+		}
+		else if (settings.cornerSwitch45 &&
+				rndCornerType < (cornerTypeRange = cornerTypeRange + settings.cornerChance45)) {
+			coord = rndCornerSide ? cornerOptions[i].c45 : cornerOptions[i].c54;
+		}
+		else {
+			coord = rndCornerSide ? cornerOptions[i].c35 : cornerOptions[i].c53;
+		}
+
+		coords.push(coord);
+	}
+
+	return coords;
+};
 
 main.playPreMove = async function() {
 	let preOptions = 1;
@@ -306,11 +353,9 @@ main.treeJumpedCheckListener = function(event) {
 	if (event.navChange) {
 		stats.clearVisits();
 
-		let currentMove = board.editor.getCurrent();
-		if ((board.lastMove.moveNumber+1 != currentMove.moveNumber ||
-				board.lastMove.navTreeY != currentMove.navTreeY)) {
-			
+		if (!event.treeChange) {
 			main.suggestions = null;
+			let currentMove = board.editor.getCurrent();
 			if (main.suggestionsHistory[currentMove.navTreeY]) {
 				main.suggestions = main.suggestionsHistory[currentMove.navTreeY][currentMove.moveNumber];
 			}
@@ -322,7 +367,7 @@ main.treeJumpedCheckListener = function(event) {
 
 			main.givePlayerControl(false);
 	
-			if (!event.treeChange && !main.isJumped) {
+			if (!main.isJumped) {
 				main.isJumped = true;
 				main.isPassed = false;
 				board.nextButton.disabled = true;
