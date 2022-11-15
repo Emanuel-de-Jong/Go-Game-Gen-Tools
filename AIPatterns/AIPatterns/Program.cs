6d45ca418a8b6fd19ffcb0a9f8f6071f74067d44
Emanuel-de-Jong <emanueldejong@hotmail.com>
2022-11-15 10:37:06 +0100

    Remove redundent code

--- AIPatterns/AIPatterns/Program.cs
+++ AIPatterns/AIPatterns/Program.cs
@@ -26,8 +26,6 @@ namespace AIPatterns
                 @"E:\Media\Documents\MEGAsync\Go\Learning\AI-Josekis\1s with first stone 5"
                 });
 
-            //StoneTreeNode StoneTree = TreeBuilder.SequenceListToStoneTree(sequenceList);
-
             GameWrap game = TreeBuilder.SequenceListToGame(sequenceList);
             
             //TreeBuilder.FilterByCount(game.Game.RootNode, 6);
@@ -72,30 +70,10 @@ namespace AIPatterns
         void Test()
         {
             GameWrap game = GameUtils.OpenFile(@"E:\Media\Downloads\test.sgf");
-            game.PlaceStone(new Stone(20, 20, true));
-            game.PlaceStone(new Stone(6, 14, false));
-            game.Game.currentNode.EnsureMarkup();
-            game.Game.currentNode.Markup.Marks.Add(new Mark(17, 17, MarkType.Square));
+
+
+
             game.SaveAsSgf(@"E:\Media\Downloads\test2.sgf");
-
-
-
-            //    GameWrap game = new GameWrap();
-            //    game.PlaceStone(new Stone(3, 6, true));
-
-            //    GameUtils.PrintBoard(game);
-            //    GameUtils.PrintStones(game);
-
-            //    for (int i = 0; i < 4; i++)
-            //    {
-            //        GameUtils.PrintBoard(game);
-            //        game = GameUtils.Rotate(game);
-            //    }
-
-            //    GameUtils.PrintBoard(game);
-            //    GameUtils.PrintBoard(GameUtils.Flip(game, true));
-            //    GameUtils.PrintBoard(GameUtils.Flip(game, false));
-            //    GameUtils.PrintBoard(GameUtils.Flip(GameUtils.Flip(game, true), false));
         }
 
     }
