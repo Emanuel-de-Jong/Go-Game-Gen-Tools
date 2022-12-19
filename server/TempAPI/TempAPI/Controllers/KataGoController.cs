d55c1cc1dc34ad960b6525e69e6205a6d14087e5
Emanuel-de-Jong <emanueldejong@hotmail.com>
2022-12-19 19:45:51 +0100

    Only log on Debug

--- server/TempAPI/TempAPI/Controllers/KataGoController.cs
+++ server/TempAPI/TempAPI/Controllers/KataGoController.cs
@@ -23,28 +23,28 @@ namespace TempAPI.Controllers
         [HttpGet("restart")]
         public void GetRestart()
         {
-            Console.WriteLine("Restart");
+            if (G.Log) Console.WriteLine("Restart");
             kataGo.Restart();
         }
 
         [HttpGet("setboardsize")]
         public void GetSetBoardsize([RegularExpression(@"(9|13|19)")] string boardsize)
         {
-            Console.WriteLine("SetBoardsize " + boardsize);
+            if (G.Log) Console.WriteLine("SetBoardsize " + boardsize);
             kataGo.SetBoardsize(int.Parse(boardsize));
         }
 
         [HttpGet("setruleset")]
         public void GetSetRuleset([RegularExpression(@"(Japanese|Chinese)")] string ruleset)
         {
-            Console.WriteLine("SetRuleset " + ruleset);
+            if (G.Log) Console.WriteLine("SetRuleset " + ruleset);
             kataGo.SetRuleset(ruleset);
         }
 
         [HttpGet("setkomi")]
         public void GetSetKomi([Range(-150, 150)] float komi)
         {
-            Console.WriteLine("SetKomi " + komi);
+            if (G.Log) Console.WriteLine("SetKomi " + komi);
             kataGo.SetKomi(komi);
         }
 
@@ -52,7 +52,7 @@ namespace TempAPI.Controllers
         public MoveSuggestion PostAnalyzeMove([RegularExpression(@"(B|W)")] string color,
             [RegularExpression(@"([A-H]|[J-T])(1[0-9]|[1-9])")] string coord)
         {
-            Console.WriteLine("AnalyzeMove " + color + " " + coord);
+            if (G.Log) Console.WriteLine("AnalyzeMove " + color + " " + coord);
             MoveSuggestion output = kataGo.AnalyzeMove(color, coord);
             //MoveSuggestion output = new(color, coord, 200, 0.8f, 1.5f);
             return output;
@@ -64,7 +64,7 @@ namespace TempAPI.Controllers
             [Range(0, 100)] float minVisitsPerc,
             [Range(0, 100)] float maxVisitDiffPerc)
         {
-            Console.WriteLine("Analyze " + color + " " + maxVisits + " " + minVisitsPerc + " " + maxVisitDiffPerc);
+            if (G.Log) Console.WriteLine("Analyze " + color + " " + maxVisits + " " + minVisitsPerc + " " + maxVisitDiffPerc);
             List<MoveSuggestion> output = kataGo.Analyze(color, maxVisits, minVisitsPerc, maxVisitDiffPerc);
             //List<MoveSuggestion> output = new()
             //{
@@ -78,21 +78,21 @@ namespace TempAPI.Controllers
         public void GetPlay([RegularExpression(@"(B|W)")] string color,
             [RegularExpression(@"([A-H]|[J-T])(1[0-9]|[1-9])")] string coord)
         {
-            Console.WriteLine("Play " + color + " " + coord);
+            if (G.Log) Console.WriteLine("Play " + color + " " + coord);
             kataGo.Play(color, coord);
         }
 
         [HttpPost("setboard")]
         public void PostSetBoard([FromBody] Moves moves)
         {
-            Console.WriteLine("SetBoard " + moves);
+            if (G.Log) Console.WriteLine("SetBoard " + moves);
             kataGo.SetBoard(moves);
         }
 
         [HttpGet("sgf")]
         public void GetSGF()
         {
-            Console.WriteLine("SGF");
+            if (G.Log) Console.WriteLine("SGF");
             kataGo.SGF();
         }
     }
