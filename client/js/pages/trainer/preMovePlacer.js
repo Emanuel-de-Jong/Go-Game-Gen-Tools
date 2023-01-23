01d68a5c2033f226de9a8e64c3646d7895d97675
Emanuel-de-Jong <emanueldejong@hotmail.com>
2023-01-23 15:36:54 +0100

    Set settings and restart after pre moves

--- client/js/pages/trainer/preMovePlacer.js
+++ client/js/pages/trainer/preMovePlacer.js
@@ -41,6 +41,10 @@ preMovePlacer.start = async function () {
 		}
 	}
 
+	await katago.sgf();
+	init.clear();
+	return;
+
 	while (G.color != board.getNextColor() && !G.isPassed && !sgf.isSGFLoading) {
 		await preMovePlacer.play(true);
 	}
